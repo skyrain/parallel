32c2936bb9e8b0644a96e737afbc5ffb4333ce13
skyrain <skyrain@skyrain-ThinkPad-X230.(none)>
2013-12-14 01:41:01 -0700

    complete serial

--- project/k_means_serial.c
+++ project/k_means_serial.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<math.h>
 
-#define N 100
+#define N 10
 #define K 5
 #define THRESHOLD 0.1
 #define RANGE 1000
@@ -19,6 +19,34 @@ Point objects[N];
 Point clusters[K];
 //---- array of object memberships ---
 int membership[N];
+
+//--- output results ----------------
+void printcluster()
+{
+    int i,j;
+    for(i = 0; i < K; i++)
+    {
+        printf("Cluster %d\n", i);
+        int row_num = 20;
+        for(j = 0; j < N; j++)
+        {
+            if(membership[j] == i)
+            {
+                if(row_num >= 0)
+                    printf("%d ", j);
+                else
+                {
+                    row_num = 20;
+                    printf("\n");
+                    printf("%d ", j);
+                }
+                row_num --;
+            }
+        }
+        printf("\n");
+    }
+}
+
 
 //---- generate random objects ------
 void initialize_object()
@@ -37,7 +65,7 @@ double get_dist(Point p1, Point p2)
 	double dist;
 	double tmp_1 = pow(p1.x - p2.x, 2);
 	double tmp_2 = pow(p1.y - p2.y, 2);
-	dist = abs(pow(tmp_1 + tmp_2, 0.5));
+	dist = fabs(sqrt(tmp_1 + tmp_2));
 	return dist;
 }
 
@@ -56,15 +84,39 @@ k_means_clustering()
 	} 
 	//---- intialize membership ------------- 
 	for(i = 0; i < N; i++)
-		membership[i] = 0;	
+    {
+        if(i < K)
+            membership[i] = i;
+        else
+        {
 
+            double dist_min = get_dist(objects[i], clusters[0]);
+            int n = 0;
+            for(j = 0; j < K; j++)
+            {
+                double dist = get_dist(objects[i], clusters[j]);
+                if(dist < dist_min)
+                {
+                    dist_min = dist;
+                    n = j;
+                }             
+            }
+            if (membership[i] != n)
+            {
+                delta += 1;
+                membership[i] = n;
+            }
+        }
+    }
+
+    delta = N;
 	while(delta / (double)N > THRESHOLD)
 	{
 		delta = 0.0;
-
+        //--- calculate to update each obj's membership ----
 		for(i = 0; i < N; i++)
 		{
-			dist_min = get_dist(objects[i], clusters[0]);
+			double dist_min = get_dist(objects[i], clusters[0]);
 			int n = 0;
 			for(j = 0; j < K; j++)
 			{
@@ -81,8 +133,28 @@ k_means_clustering()
 				membership[i] = n;
 			}
 		}
-
-		//--- update to get new centers ---
+		
+        //--- update to get new centers for each cluster---
+        for(i = 0; i < K; i++)
+        {
+            double new_center_x = 0;
+            double new_center_y = 0; 
+            int cluster_num = 0;
+            for(j = 0; j < N; j++)
+            {
+                if(membership[j] == i)
+                {
+                    new_center_x += objects[j].x;
+                    new_center_y += objects[j].y;
+                    cluster_num ++;
+                }
+            }
+            clusters[i].x = new_center_x;
+            clusters[i].y = new_center_y;
+        }       
+        
+        //-- printf the resulting cluster ------
+        printcluster();   
 	}
 
 }
@@ -90,6 +162,7 @@ k_means_clustering()
 
 int main()
 {
+    k_means_clustering();
 
 	return 0;    
 }
